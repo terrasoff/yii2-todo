b55cfe097090fc8141112ff4de9b22bb93573446
terrasoff <terrasoff@terrasoff.ru>
2014-03-27 22:46:11 +0400

    host

--- config/db.php
+++ config/db.php
@@ -1,14 +1,15 @@
 <?php
-$path = __DIR__.DIRECTORY_SEPARATOR.'local.php';
-if (file_exists($path))
-    require($path);
+$file = __DIR__.DIRECTORY_SEPARATOR.'local.php';
+if (file_exists($file))
+    require($file);
 
+defined('MYSQL_HOST') or define('MYSQL_HOST', 'localhost');
 defined('MYSQL_USER') or define('MYSQL_USER', 'root');
 defined('MYSQL_PASS') or define('MYSQL_PASS', 'password');
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=test',
+    'dsn' => 'mysql:host=localhost;dbname='.MYSQL_HOST,
     'username' => MYSQL_USER,
     'password' => MYSQL_PASS,
     'charset' => 'utf8',
